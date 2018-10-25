9c7f634d54e0156830fc686600a782473339da0f
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-10-25 11:46:46 +0530

    makeConstant passed

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,38 @@
-const makeConstant = undefined;
+const makeConstant = function(input){
+  const constantlyReturnTwo = function(input){
+    return 2;
+  }
+  const constantlyReturnString = function(input){
+    return 'hello';
+  }
+  const constantlyReturnArray = function(input){
+    return [1,2,3];
+  }
+  const constantlyReturnObject = function(input){
+    return {a: 1, b: 2};
+  }
+  if ( input == 2){
+    return constantlyReturnTwo;
+  }
+  else if ( input.toString() == 'hello'){
+    return constantlyReturnString;
+  }
+  else if (input.toString() == '1,2,3'){
+    return constantlyReturnArray;
+  }
+  else {
+    return constantlyReturnObject;
+  }
+}
+
+
+
+
+
+
+
+
+
 const makeCounterFromN = undefined;
 const makeCounterFromZero = undefined;
 const makeDeltaTracker = undefined;
